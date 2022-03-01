ab3fe42a76b1179addaa128c48fcd8fe8ea1f8b2
yashwanth06062000 <yashwanthkrishna150@gmail.com>
2022-03-01 21:37:04 +0530

    database used .env

--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'Thisonlyme@1', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   dialect: 'mysql',
-  host: 'localhost'
+  host: process.env.DB_HOST
 });
 
 module.exports = sequelize;
