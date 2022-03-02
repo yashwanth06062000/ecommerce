5cdff6f3d993de1cbfc5c2bd8c42057483be1f34
yashwanth06062000 <yashwanthkrishna150@gmail.com>
2022-03-02 09:46:31 +0530

    added dotenvfile in database.js

--- util/database.js
+++ util/database.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
-
+const dotenv=require('dotenv')
+dotenv.config()
+console.log(process.env)
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   dialect: 'mysql',
   host: process.env.DB_HOST
